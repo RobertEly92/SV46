a9dbd9b7795cafa4deb8fad14967a1e8ae4373e5
RobertKappler <robert@kapplers.de>
2024-06-05 20:48:21 +0200

    setup for variable use in eventListElement

--- sv46_rosenow/test/widget_tests/eventListElement_test.dart
+++ sv46_rosenow/test/widget_tests/eventListElement_test.dart
@@ -4,17 +4,24 @@ import 'widget_tests_setup.dart';
 
 void main() {
   testWidgets('test if all elements are visible', (WidgetTester tester) async {
-    
-    //TODO chekcn warum setuptester lokalization nicht mehr ltr problem löst
-    await setupTester(tester, const EventListElement_widget());
+  
+    const String t_headline = 'Headline';
+    const String t_startTime = '18:30';
+    const String t_location = 'Am Fettlock 8';
+    const String t_registratedPeople = '11';
+    const String t_meetingTime = '18:20';
 
+    //TODO chekcn warum setuptester lokalization nicht mehr ltr problem löst
+    await setupTester(tester, const EventListElement_widget(eventHeadline: t_headline, eventStartTime: t_startTime, eventLocation: t_location, eventRegistratedPeople: t_registratedPeople,
+    eventMeetingTime: t_meetingTime));
 
     //TODO gegen variablen ersetzen
     // finders
-    final headline = find.text('Headline');
-    final time = find.text('18:30');
-    final location = find.text('Am Fettloch 8');
-    final registratedPeople = find.text('11');
+    final headline = find.text(t_headline);
+    final time = find.text(t_startTime);
+    final location = find.text(t_location);
+    final registratedPeople = find.text(t_registratedPeople);
+    final meetingTime = find.text(t_meetingTime);
 
     //check if state is correct
     expect(headline, findsOneWidget);
